05feaab6bf31923acb3ef851cf31d4e23a95044c
santtuniskanen <santtuniska02@gmail.com>
2022-12-19 11:49:47 +0200

    U

---
+++ README.md
@@ -0,0 +1,4 @@
+# Unit Testing with Python
+
+The purpose of this repository is to learn
+how to do Unit Testing using Python.
---
+++ examplecalculator/test_calc.py
@@ -0,0 +1,27 @@
+import unittest
+import calc
+
+class TestCalc(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(calc.add(1, 2), 3)
+        self.assertEqual(calc.add(5, 6), 11)
+        self.assertEqual(calc.add(-1, -1), -2)
+
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(1, 2), -1)
+        self.assertEqual(calc.subtract(5, 6), -1)
+        self.assertEqual(calc.subtract(-1, -1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(1, 2), 2)
+        self.assertEqual(calc.multiply(5, 6), 30)
+        self.assertEqual(calc.multiply(-1, -1), 1)
+
+    def test_divide(self):
+        self.assertEqual(calc.divide(1, 2), 0.5)
+        self.assertEqual(calc.divide(5, 1), 5)
+        self.assertEqual(calc.divide(-1, -1), 1)
+
+if __name__ == '__main__':
+    unittest.main()
